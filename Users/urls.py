692ba7f5101edc30eeaec3e6446d2e63b98d741c
Masma175 <jephtemasma8@gmail.com>
2022-05-17 19:21:14 +0200

    Finition de la Vue activation

--- Users/urls.py
+++ Users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import dashboard, loginView
+from .views import dashboard, loginView, activate, signup
 
 
 
 urlpatterns = [
     path('', loginView, name='login'),
-    path('user/dashboard', dashboard, name="dashboard")
+    path('user/dashboard', dashboard, name="home"),
+    path('register/', signup, name = 'register'),
+    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/', activate, name='activate'), 
 ]
--- user_authentification/settings.py
+++ user_authentification/settings.py
@@ -81,6 +81,13 @@ DATABASES = {
     }
 }
 
+#Email configuration
+
+EMAIL_USE_TLS = True  
+EMAIL_HOST = 'smtp.gmail.com'  
+EMAIL_HOST_USER = 'jephtemasma8@gmail.com'  
+EMAIL_HOST_PASSWORD = 'swuoizamrgyjdpuc'  
+EMAIL_PORT = 587
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
