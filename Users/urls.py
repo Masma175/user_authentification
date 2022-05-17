79fb89c970b252a12afe0005fcc7925ffa0af31e
Masma175 <jephtemasma8@gmail.com>
2022-05-17 21:09:19 +0200

    f

--- Users/urls.py
+++ Users/urls.py
@@ -5,7 +5,7 @@ from .views import dashboard, loginView, activate, signup
 
 urlpatterns = [
     path('', loginView, name='login'),
-    path('user/dashboard', dashboard, name="home"),
+    path('user/dashboard', dashboard, name="dashboard"),
     path('register/', signup, name = 'register'),
     path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/', activate, name='activate'), 
 ]
--- user_authentification/settings.py
+++ user_authentification/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
 
 EMAIL_USE_TLS = True  
 EMAIL_HOST = 'smtp.gmail.com'  
-EMAIL_HOST_USER = 'jephtemasma8@gmail.com'  
+EMAIL_HOST_USER = 'test@gmail.com'  
 EMAIL_HOST_PASSWORD = 'swuoizamrgyjdpuc'  
 EMAIL_PORT = 587
 
