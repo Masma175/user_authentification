2ac6aaa34f40a38b295eb886dbe3a16d8c51e40a
Masma175 <jephtemasma8@gmail.com>
2022-05-17 07:18:45 +0200

    Ajout de la Vue login

--- Users/views.py
+++ Users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login
 # Create your views here.
+
+
+def loginView(request):
+    """ Vue de l'authentification (connexion de l'utilisateur) """
+    error = ""
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user = authenticate(username=username, password=password)#Vérification de l'authenticité de l'utilisateur
+
+    if user is not None:
+        login(request, user)
+        return redirect("profile")
+    else:
+        error = "Nom d'utilisateur ou mot de passe incorrect"
+    
+    context = {
+        'error': error,
+    }
+    return render(request, 'users/login.html', context)
